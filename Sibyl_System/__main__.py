351ecc91104c915a8acf290b2c84bf7b454e017a
AzeonScanner <93833994+AzeonScanner@users.noreply.github.com>
2021-11-07 11:51:52 +0530

    Update __main__.py

--- Sibyl_System/__main__.py
+++ Sibyl_System/__main__.py
@@ -41,7 +41,7 @@ for load in to_load:
         print("------------------------------------")
 
 
-@System.on(system_cmd(pattern=r"exinfo", allow_enforcer=True))
+@System.on(system_cmd(pattern=r"axinfo", allow_enforcer=True))
 async def status(event):
     msg = await event.reply("Conecting to Azeon ✣ Core.")
     time.sleep(1)
